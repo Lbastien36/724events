9d70aecf2c6724a08f08e23a289ae4c229138e0e
Bastien <bastien.lasne@outlook.fr>
2024-05-01 15:55:30 +0200

    La modale d'envoie s'affiche désormais

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -15,7 +15,8 @@ const Form = ({ onSuccess, onError }) => {
       // We try to call mockContactApi
       try {
         await mockContactApi();
-        setSending(false);
+        setSending(false)
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
